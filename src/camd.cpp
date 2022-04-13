3c282d199f92635d2fa0a3ad8ab6ee074755636c
Math-42 <matheus.v4200@gmail.com>
2022-04-12 22:54:18 -0300

    camd: add the setup of the service

--- src/camd.cpp
+++ src/camd.cpp
@@ -8,11 +8,49 @@
 #include <string>
 
 class CamdService : public RoutineService {
+    static cv::Mat frame;
+    static cv::VideoCapture cap;
+    int camTurningOnDelay = 5;
+    std::string absoluteFilePath = "./";
+    int camPhotoDelay = 5000;
+
+    const DBusHandler::Path takePhotoPath {
+        "zfkd.dbus.camd",
+        "zfkd/dbus/camd",
+        "zfkd.dbus.camd",
+        "takePhoto"
+    };
+
 public:
     explicit CamdService(std::string serviceId)
         : RoutineService { serviceId }
     {
     }
+
+    void setup() override
+    {
+        cap.open(0, cv::CAP_ANY);
+
+        if (!cap.isOpened()) {
+            std::cerr << "ERRO! Camêra inacessível\n";
+        }
+
+        DBusHandler::registerMethod(takePhotoPath, [&](nlohmann::json messaWrapper) {
+            nlohmann::json response;
+
+            try {
+                takePhoto(messaWrapper["filePath"]);
+                response["error"] = false;
+            } catch (...) {
+                response["error"] = true;
+            }
+
+            return response;
+        });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(camTurningOnDelay));
+    }
+
     void destroy() override
     {
     }
